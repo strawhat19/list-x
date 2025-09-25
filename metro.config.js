4bd569e94a41898bb8a1ef9414f775e90b3d08a9
rakib <rahmed@hvac.mea.com>
2025-09-24 20:00:32 -0400

    List X Init

---
+++ metro.config.js
@@ -0,0 +1,16 @@
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-sass-transformer'),
+};
+
+config.resolver = {
+  ...config.resolver,
+  sourceExts: [...config.resolver.sourceExts, 'scss', 'sass'],
+};
+
+module.exports = config;
